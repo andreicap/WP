150990aa7969ea9546dc496fe8be48dcbd88fe4f
Andrew <andreicap@me.com>
2014-02-11 12:15:25 +0200

    Create simple window

--- lab#1/main.cpp
+++ lab#1/main.cpp
@@ -1,79 +1,81 @@
 #include <windows.h>
 
-/*  Declare Windows procedure  */
-LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WinProc(HWND hWnd,UINT message,WPARAM wParam,LPARAM lParam);
 
-/*  Make the class name into a global variable  */
-char szClassName[ ] = "CodeBlocksWindowsApp";
+int WINAPI WinMain(HINSTANCE hInst,HINSTANCE hPrevInst,LPSTR lpCmdLine,int nShowCmd)
+{
+	WNDCLASSEX wClass;
+	ZeroMemory(&wClass,sizeof(WNDCLASSEX));
+	wClass.cbClsExtra=NULL;
+	wClass.cbSize=sizeof(WNDCLASSEX);
+	wClass.cbWndExtra=NULL;
+	wClass.hbrBackground=(HBRUSH)COLOR_WINDOW;
+	wClass.hCursor=LoadCursor(NULL,IDC_ARROW);
+	wClass.hIcon=NULL;
+	wClass.hIconSm=NULL;
+	wClass.hInstance=hInst;
+	wClass.lpfnWndProc=(WNDPROC)WinProc;
+	wClass.lpszClassName="Window Class";
+	wClass.lpszMenuName=NULL;
+	wClass.style=CS_HREDRAW|CS_VREDRAW;
 
-int WINAPI WinMain (HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nCmdShow){
-  HWND hwnd;               /* This is the handle for our window */
-  MSG messages;            /* Here messages to the application are saved */
-  WNDCLASSEX wincl;        /* Data structure for the windowclass */
+	if(!RegisterClassEx(&wClass))
+	{
+		int nResult=GetLastError();
+		MessageBox(NULL,
+			"Window class creation failed",
+			"Window Class Failed",
+			MB_ICONERROR);
+	}
 
-  /* The Window structure */
-  wincl.hInstance = hThisInstance;
-  wincl.lpszClassName = szClassName;
-  wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
-  wincl.style = CS_DBLCLKS;                 /* Catch double-clicks */
-  wincl.cbSize = sizeof (WNDCLASSEX);
+	HWND hWnd=CreateWindowEx(NULL,
+			"Window Class",
+			"Windows application",
+			WS_OVERLAPPEDWINDOW,
+			200,
+			200,
+			640,
+			480,
+			NULL,
+			NULL,
+			hInst,
+			NULL);
 
-  /* Use default icon and mouse-pointer */
-  wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
-  wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
-  wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
-  wincl.lpszMenuName = NULL;                 /* No menu */
-  wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
-  wincl.cbWndExtra = 0;                      /* structure or the window instance */
-  /* Use Windows's default colour as the background of the window */
-  wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+	if(!hWnd)
+	{
+		int nResult=GetLastError();
 
-  /* Register the window class, and if it fails quit the program */
-  if (!RegisterClassEx (&wincl))
-    return 0;
+		MessageBox(NULL,
+			"Window creation failed",
+			"Window Creation Failed",
+			MB_ICONERROR);
+	}
 
-  /* The class is registered, let's create the program*/
-  hwnd = CreateWindowEx (
-    0,                   /* Extended possibilites for variation */
-    szClassName,         /* Classname */
-    "WP Lab#1 example",  /* Title Text */
-    WS_OVERLAPPEDWINDOW, /* default window */
-    CW_USEDEFAULT,       /* Windows decides the position */
-    CW_USEDEFAULT,       /* where the window ends up on the screen */
-    544,                 /* The programs width */
-    375,                 /* and height in pixels */
-    HWND_DESKTOP,        /* The window is a child-window to desktop */
-    NULL,                /* No menu */
-    hThisInstance,       /* Program Instance handler */
-    NULL                 /* No Window Creation data */
-  );
+	ShowWindow(hWnd,nShowCmd);
 
-  /* Make the window visible on the screen */
-  ShowWindow (hwnd, nCmdShow);
+	MSG msg;
+	ZeroMemory(&msg,sizeof(MSG));
 
-  /* Run the message loop. It will run until GetMessage() returns 0 */
-  while (GetMessage (&messages, NULL, 0, 0)){
-    /* Translate virtual-key messages into character messages */
-    TranslateMessage(&messages);
-    /* Send message to WindowProcedure */
-    DispatchMessage(&messages);
-  }
+	while(GetMessage(&msg,NULL,0,0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
-  /* The program return-value is 0 - The value that PostQuitMessage() gave */
-  return messages.wParam;
+	return 0;
 }
 
+LRESULT CALLBACK WinProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam)
+{
+	switch(msg)
+	{
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			return 0;
+		}
+		break;
+	}
 
-/*  This function is called by the Windows function DispatchMessage()  */
-
-LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam){
-  switch (message){                  /* handle the messages */
-    case WM_DESTROY:
-      PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
-      break;
-    default:                      /* for messages that we don't deal with */
-      return DefWindowProc (hwnd, message, wParam, lParam);
-  }
-
-  return 0;
+	return DefWindowProc(hWnd,msg,wParam,lParam);
 }
