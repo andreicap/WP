b85ecc37c311827f6a468175ce8575067886743e
Andrew <andreicap@me.com>
2014-02-11 13:34:58 +0200

    Output text with paintStruct

--- lab#1/main.cpp
+++ lab#1/main.cpp
@@ -1,4 +1,7 @@
 #include <windows.h>
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #define IDC_MAIN_BUTTON	101			// Button identifier
 #define IDC_MAIN_EDIT	102			// Edit box identifier
@@ -71,105 +74,109 @@ int WINAPI WinMain(HINSTANCE hInst,HINSTANCE hPrevInst,LPSTR lpCmdLine,int nShow
 
 LRESULT CALLBACK WinProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam)
 {
+    HDC hdc;
+    PAINTSTRUCT ps;
+    RECT rect;
     static HWND hEdit;
+    static char buffer[256];
 	switch(msg)
 	{
-		case WM_CREATE:
-		{
-			// Create an edit box
-			hEdit=CreateWindowEx(WS_EX_CLIENTEDGE,
-				"EDIT",
-				"",
-				WS_CHILD|WS_VISIBLE|
-				ES_MULTILINE|ES_AUTOVSCROLL|ES_AUTOHSCROLL,
-				50,
-				100,
-				200,
-				100,
-				hWnd,
-				(HMENU)IDC_MAIN_EDIT,
-				GetModuleHandle(NULL),
-				NULL);
-			HGDIOBJ hfDefault=GetStockObject(DEFAULT_GUI_FONT);
-			SendMessage(hEdit,
-				WM_SETFONT,
-				(WPARAM)hfDefault,
-				MAKELPARAM(FALSE,0));
-			SendMessage(hEdit,
-				WM_SETTEXT,
-				NULL,
-				(LPARAM)"Enter your name here...");
-
-			// Create a push button
-			HWND hWndButton=CreateWindowEx(NULL,
-				"BUTTON",
-				"OK",
-				WS_TABSTOP|WS_VISIBLE|
-				WS_CHILD|BS_DEFPUSHBUTTON,
-				50,
-				220,
-				100,
-				24,
-				hWnd,
-				(HMENU)IDC_MAIN_BUTTON,
-				GetModuleHandle(NULL),
-				NULL);
-			SendMessage(hWndButton,
-				WM_SETFONT,
-				(WPARAM)hfDefault,
-				MAKELPARAM(FALSE,0));
+        case WM_CREATE:
+        {
+            // Create an edit box
+            hEdit=CreateWindowEx(WS_EX_CLIENTEDGE,
+                "EDIT",
+                "",
+                WS_CHILD|WS_VISIBLE|
+                ES_MULTILINE|ES_AUTOVSCROLL,
+                50,
+                100,
+                200,
+                100,
+                hWnd,
+                (HMENU)IDC_MAIN_EDIT,
+                GetModuleHandle(NULL),
+                NULL);
+            HGDIOBJ hfDefault=GetStockObject(DEFAULT_GUI_FONT);
+            SendMessage(hEdit,
+                WM_SETFONT,
+                (WPARAM)hfDefault,
+                MAKELPARAM(FALSE,0));
+            SendMessage(hEdit,
+                WM_SETTEXT,
+                NULL,
+                (LPARAM)"Enter your name here...");
+            // Create a push button
+            HWND hWndButton=CreateWindowEx(NULL,
+                "BUTTON",
+                "OK",
+                WS_TABSTOP|WS_VISIBLE|
+                WS_CHILD|BS_DEFPUSHBUTTON,
+                50,
+                220,
+                100,
+                24,
+                hWnd,
+                (HMENU)IDC_MAIN_BUTTON,
+                GetModuleHandle(NULL),
+                NULL);
+            SendMessage(hWndButton,
+                WM_SETFONT,
+                (WPARAM)hfDefault,
+                MAKELPARAM(FALSE,0));
 
             HWND hWndExit=CreateWindowEx(NULL,
-				"BUTTON",
-				"EXIT",
-				WS_TABSTOP|WS_VISIBLE|
-				WS_CHILD|BS_DEFPUSHBUTTON,
-				300,
-				400,
-				50,
-				24,
-				hWnd,
-				(HMENU)IDC_EXIT_BUTTON,
-				GetModuleHandle(NULL),
-				NULL);
-			SendMessage(hWndButton,
-				WM_SETFONT,
-				(WPARAM)hfDefault,
-				MAKELPARAM(FALSE,0));
-		}
-		break;
+                "BUTTON",
+                "EXIT",
+                WS_TABSTOP|WS_VISIBLE|
+                WS_CHILD|BS_DEFPUSHBUTTON,
+                270,
+                350,
+                100,
+                60,
+                hWnd,
+                (HMENU)IDC_EXIT_BUTTON,
+                GetModuleHandle(NULL),
+                NULL);
+            SendMessage(hWndButton,
+                WM_SETFONT,
+                (WPARAM)hfDefault,
+                MAKELPARAM(FALSE,0));
+        }break;
+        case WM_COMMAND:
+            switch(LOWORD(wParam))
+            {
 
-		case WM_COMMAND:
-			switch(LOWORD(wParam))
-            {
-            case IDC_MAIN_BUTTON:
-            {
-                char buffer[256];
-                SendMessage(hEdit,
-                WM_GETTEXT,
-                sizeof(buffer)/sizeof(buffer[0]),
-                reinterpret_cast<LPARAM>(buffer));
-                MessageBox(NULL,
-                buffer,
-                "Your name",
-                MB_ICONQUESTION | MB_YESNO);
-            }
-            break;
-            case IDC_EXIT_BUTTON:
-            {
-                PostQuitMessage(0);
-                return 0;
-            }
-            break;
-        }
-        break;
+                case IDC_MAIN_BUTTON:
+                {
+                    SendMessage(hEdit,
+                    WM_GETTEXT,
+                    sizeof(buffer)/sizeof(buffer[0]),
+                    reinterpret_cast<LPARAM>(buffer));
+                    //MessageBox(NULL, buffer,"Your name",MB_ICONQUESTION | MB_YESNO);
+                }break;
 
-		case WM_DESTROY:
-		{
-			PostQuitMessage(0);
-			return 0;
-		}
-		break;
+                case IDC_EXIT_BUTTON:
+                {
+                    PostQuitMessage(0);
+                    return 0;
+                }break;
+        }break;
+
+        case WM_PAINT:
+        {
+            hdc=BeginPaint(hWnd, &ps);
+            GetClientRect(hWnd, &rect);
+            DrawText(hdc, "something here",-1,&rect, DT_SINGLELINE  | DT_RIGHT );
+            EndPaint(hWnd, &ps);
+        }break;
+
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+            return 0;
+        }break;
+
 	}
 
 	return DefWindowProc(hWnd,msg,wParam,lParam);
